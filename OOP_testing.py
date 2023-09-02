5e4e5ba1ff53199fb6749a5f67fc2142ea9930f5
Adi Cohen <adi745@gmail.com>
2023-09-02 16:30:36 +0300

    started implmenting instance and class attributes also started implementing the total_working_time of machines

--- OOP_testing.py
+++ OOP_testing.py
@@ -1,24 +1,46 @@
 class PlantArea:
-    def __init__(self, name): #creating a constractor, it's one of the magic methods
-        print(f"An area instance created: {name}") #creating uniqe identifier for indication where it came from with the barckets
+    # class attribute
+    total_operation_time = 0.0
+    number_of_control_panels = 3 #number of control panels which are attached to each machine
+    def __init__(self, name: str, num_of_machines = 5, machine_x_size = 300.0, machine_y_size = 253.5): #creating a constractor, it's one of the magic methods
+        # print(f"An area instance created: {name}") #creating unique identifier for indication where it came from with the barckets
+        # run validations to the received arguments
+        assert num_of_machines >= 0, f"number of machines:{num_of_machines} must be bigger than 0!" #way to check that we comply the expectations we have
+        assert machine_x_size > 0.0, f"machine dimensions:{machine_x_size} must be bigger than 0.0" #passing assertion message for the test
+        assert machine_y_size > 0.0, f"machine dimensions:{machine_y_size} must be bigger than 0.0"
 
-    def calculate_total_used_space(self, machine_x_size, machine_y_size, num_of_machines): #we have to send at least one parameter on every method and the 
-        return (machine_x_size/100.0) * (machine_y_size/100.0) * num_of_machines
-        # common convention is self
+        # assign to self object, these are instance attributes!!!
+        self.name = name #now we don't need to assign name from outside, we assign it from the constractor
+        self.num_of_machines = num_of_machines
+        self.machine_x_size = machine_x_size
+        self.machine_y_size = machine_y_size
+        # when we pass a default value we also declare the variable type we want to pass
+    def calculate_total_used_space(self): #we have to send at least one parameter on every method and the 
+        return (self.machine_x_size/100.0) * (self.machine_y_size/100.0) * self.num_of_machines
+        # common convention is self, here is the main power of using the self we pass with it's atttributes
         
+    def calculate_total_operation_time(self):
+        pass
 
-plant1 = PlantArea("area1")
-plant1.num_of_machines = 12
-plant1.machine_x_size = 300.0 #in cm
-plant1.machine_y_size = 253.5 #in cm
+plant_area1 = PlantArea("area1")
+# plant_area1.num_of_machines = 12
+# plant_area1.machine_x_size = 300.0 #in cm
+# plant_area1.machine_y_size = 253.5 #in cm
 
-plant2 = PlantArea("area2")
-plant2.num_of_machines = 65
-plant2.machine_x_size = 500.0 #in cm
-plant2.machine_y_size = 357.5 #in cm
+plant_area2 = PlantArea("area2", 65, 500.0, 357.5)
+# plant_area2.num_of_machines = 65
+# plant_area2.machine_x_size = 500.0 #in cm
+# plant_area2.machine_y_size = 357.5 #in cm
 
-# print(type(plant1))
-# print(type(plant1.area))
-# print(type(plant1.num_of_machines))
-# print(plant1.calculate_total_used_space(plant1.machine_x_size, plant1.machine_y_size, plant1.num_of_machines))
-# print(plant2.calculate_total_used_space(plant2.machine_x_size, plant2.machine_y_size, plant2.num_of_machines))
+#even if we use constractors we can assign additional attributes outside of them
+
+plant_area2.outside_plant = True #this attribute is not part of the constractor
+print(plant_area2.outside_plant)
+
+print(PlantArea.number_of_control_panels) #because this is a class attribute, we can get into it from the class itself
+print(plant_area1.number_of_control_panels) #because the instances weren't able to find the attribute from the 
+# instance level they searched for that in class level
+print(plant_area2.number_of_control_panels)
+
+print(PlantArea.__dict__) #print all the attributes in the class level, converting all attributes to dictionary
+print(plant_area1.__dict__) #print all the attributes in the instance level, converting all attributes to dictionary
